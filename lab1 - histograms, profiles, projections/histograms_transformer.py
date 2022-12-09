2f1a60315893ca30288f8a5188ec5367b5ab350a
Vitaliy Kurchaviy <vitalykurchaviy@yandex.ru>
2022-12-09 18:34:02 +0300

    added even_tranformation

--- lab1 - histograms, profiles, projections/histograms_transformer.py
+++ lab1 - histograms, profiles, projections/histograms_transformer.py
@@ -31,11 +31,17 @@ def linear_transform(img: ndarray, shift: float = 0) -> ndarray:
 
 
 def stretching(img: ndarray, a: float = 1) -> ndarray:
-    i_max = np.max(img)
-    i_min = np.min(img)
+    i_max, i_min = np.max(img), np.min(img)
     return (255*(np.power((img - i_min)/(i_max - i_min), a))).astype(np.uint8)
 
-# def even_transformation(img : ndarray, cum_hist: ndarray):
+
+def even_transformation(img: ndarray, cum_hist: ndarray) -> ndarray:
+    i_max, i_min = np.max(img), np.min(img)
+    new_img = ndarray(img.shape)
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            new_img[x][y] = (i_max - i_min) * cum_hist[img[x][y]] + i_min
+    return new_img.astype(np.uint8)
 
 
 def plot_histogram(hist: ndarray, subplot, title=None, label_y=None):
@@ -47,12 +53,10 @@ def plot_histogram(hist: ndarray, subplot, title=None, label_y=None):
 
 
 def plot_cum_histogram(hist: ndarray, subplot, title=None, label_y=None):
-    print(hist.shape)
-    print(hist)
     subplot.set_title(title)
     subplot.set_xlim([0, 255])
     subplot.set_ylim([0, int(np.max(hist))])
-    subplot.plot(list(range(256)), list(map(int, hist)))
+    subplot.plot(list(range(256)), list(hist))
     subplot.set_ylabel(label_y)
 
 
@@ -76,14 +80,19 @@ def plot_images_comparison(hist1: ndarray, img1: ndarray, cum_hist1: ndarray,
 
 
 def cum_histogram(hist, num_rows, num_column):
-    return 500*np.cumsum(hist) / (num_rows * num_column)
+    return np.cumsum(hist) / (num_rows * num_column)
 
 
 if __name__ == "__main__":
     image = get_image_with_min_contrast(Path(PATH_TO_PHOTO))
+    print(image)
+    print(image.shape)
     histogram = cv.calcHist([image], [0], None, [256], [0, 256])
-    image_2 = stretching(image, 1.9)
-    cum_h = cum_histogram(histogram, image.shape[0], image.shape[1])
+    cumh = cum_histogram(histogram, image.shape[0], image.shape[1])
+    image_2 = even_transformation(image, cumh)
+    print(image_2)
+    print(type(image_2))
+    print(image_2.shape)
     histogram_2 = cv.calcHist([image_2], [0], None, [256], [0, 256])
     cumh_2 = cum_histogram(histogram_2, image.shape[0], image.shape[1])
-    plot_images_comparison(histogram, image, cum_h, histogram_2, image_2, cumh_2)
+    plot_images_comparison(histogram, image, cumh, histogram_2, image_2, cumh_2)
